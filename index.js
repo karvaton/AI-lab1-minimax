eca824ff6eb3b2be8a7d59f9c67ede1e2fed3d62
Karvaton <karvaton220@gmail.com>
2023-11-19 05:13:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function initBoard() {
 // вибір сторони (візуальна складова)
 const choiceField = document.getElementById('choice');
 const choiceButtons = choiceField.getElementsByTagName('button');
-
+// взаємодія з меню для вибору за кого грати
 Array.from(choiceButtons).forEach((button, index) => {
     button.addEventListener('click', () => {
         setNextPlayer(X);
@@ -61,28 +61,35 @@ function makeMove(index) {
     setSymbol(index);
     setNextPlayer(nextPlayer === X ? O : X);
     
+    // перевірка чи завершилась гра
     const winner = checkWin(boardSymbols);
     if (winner) {
+        // оголошення переможця
         setTimeout(() => alert(`Переміг "${winner === 1 ? X : O}"`), 10);
         return;
     } else if (winner === 0) {
+        // оголощення нічиї
         setTimeout(() => alert("Нічия"), 10);
         return;
     }
 
+    // здійснення ходу комп'ютером
     if (nextPlayer !== user) {
         computerMove();
     }
+    // якщо гра не завершена і зараз не черга комп'ютера ходити, то очікуємо на хід користувача 
 }
 
 
 // функція ходу комп'ютера
 function computerMove() {
     let index;
-    board.classList.add('waiting');
+    Array.from(cells).forEach(cell => cell.classList.add('waiting'));
     setTimeout(() => {
+        // виклик функції для знаходження найкращого ходу
         index = findBestMove(nextPlayer, boardSymbols);
-        board.classList.remove('waiting');
+        Array.from(cells).forEach(cell => cell.classList.remove('waiting'));
+        // виклик функції для здійснення наступного ходу 
         makeMove(index);
     }, 500);
 }
@@ -90,36 +97,39 @@ function computerMove() {
 
 // функція для додавання символа на дошку 
 function setSymbol(index) {
-    boardSymbols[index] = nextPlayer;
-    cells[index].textContent = nextPlayer;
+    boardSymbols[index] = nextPlayer; // додаємо хід в масив
+    cells[index].textContent = nextPlayer; // додаємо символ на дошку у візульному інтерфейсі
 }
 
 
 // встановити наступного гравця
 function setNextPlayer(player) {
     nextPlayer = player;
-    message.textContent = player;
+    message.textContent = player; // додати повідомлення на екран про те хто ходитеме наступний
 }
 
 
 // алгоритм пошуку Мінімакс
 function minimax(isMax) {
+    // Базовий випадок у якому перевіряється чи досягнуто перемоги
     const result = checkWin(boardSymbols);
     if (result !== null) {
         return result;
     }
 
-    let bestScore = isMax ? -Infinity : Infinity;
+    let bestScore = isMax ? -Infinity : Infinity; // присвоєння початкового значення
     let player = isMax ? X : O;
     
+    // у циклі проходимо по кожному варіанту ходів
     for (let i = 0; i < boardSymbols.length; i++) {
         if (boardSymbols[i] === null) {
-            boardSymbols[i] = player;
+            boardSymbols[i] = player; // здійснення ходу
             const score = minimax(!isMax);
-            boardSymbols[i] = null;
-            bestScore = Math[isMax ? 'max' : 'min'](score, bestScore);
+            boardSymbols[i] = null; // скасування ходу
+            bestScore = Math[isMax ? 'max' : 'min'](score, bestScore); // переприсвоєння найкращого результату
         }
     }
+    // повертаємо результат
     return bestScore;
 }
 
@@ -128,20 +138,22 @@ function minimax(isMax) {
 function findBestMove(player, board) {
     let bestScore = player === X ? -Infinity : Infinity;
     let bestMove = -1;
-  
+    
+    // у циклі проходимо по кожному варіанту ходів
     for (let i = 0; i < board.length; i++) {
         if (board[i] === null) {
-            board[i] = player;
-            const score = minimax(player === O);
-            board[i] = null;
+            board[i] = player; // здійснення ходу
+            const score = minimax(player === O); // виклик алгоритму "мінімакс"
+            board[i] = null; // скасування ходу
   
             if (player === O ? score < bestScore : score > bestScore) {
                 bestScore = score;
-                bestMove = i;
+                bestMove = i; // присвоєння найкращого хочу
             }
         }
     }
-  
+    
+    // повертаємо найкращий хід
     return bestMove;
 }
 
@@ -159,6 +171,7 @@ function checkWin(board) {
         [2, 4, 6],
     ];
     
+    // перевірка на переможця
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
@@ -166,13 +179,17 @@ function checkWin(board) {
         }
     }
 
+    // перквірка на нічию
     if (board.includes(null))
-        return null;
+        return null; // якщо гра не завершена, повертаємо null
     return 0;
 }
 
 
-// Функція почати з початку
+/* 
+Функція почати з початку
+для візуальної версії
+*/
 function restart() {
     boardSymbols.fill(null);
     Array.from(cells).forEach(cell => cell.textContent = '');
